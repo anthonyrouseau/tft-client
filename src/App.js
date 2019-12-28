b8c1913d70a793fa43f5629c0e59c702490fb8bf
Anthony Rouseau <anthonyrouseau@Anthonys-MacBook-Pro.local>
2019-12-27 23:24:40 -0800

    first commit, building out profile card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProfileCard from './Components/ProfileCard';
+import axios from 'axios';
+
 
 function App() {
+  const [profile, setProfile] = React.useState({});
+  const [name, setName] = React.useState("Narfyy");
+  React.useEffect( () => {
+    async function fetchProfile() {
+      try {
+        var res = await axios.get(`/profile/${name}`);
+        setProfile(res.data);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    if(name !== ""){
+      fetchProfile();
+    }
+  }, [name])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossOrigin="anonymous"
+      />
+      <ProfileCard profile={profile}></ProfileCard>
     </div>
   );
 }
---
+++ src/Components/ProfileCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+
+function ProfileCard(props) {
+    let profile = props.profile;
+    return (
+        <Card style={{width: '300px'}}>
+            <Card.Header>Player Profile</Card.Header>
+            <Card.Body>
+                <Card.Title>{profile.name}</Card.Title>
+                <Card.Subtitle><Image src={profile.tierLink} fluid/>{`${profile.tier} ${profile.rank}`}</Card.Subtitle>
+                <Card.Text>Placeholder text</Card.Text>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>{`Wins: ${profile.wins}`}</ListGroup.Item>
+                    {profile.hotStreak && <ListGroup.Item>Hot Streak!</ListGroup.Item>}
+                </ListGroup>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default ProfileCard
